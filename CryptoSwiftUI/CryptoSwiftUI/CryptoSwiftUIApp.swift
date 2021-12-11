e25380a274340c378f39d5083a95b6eb9e64954f
Valery Kavaleuski <kovalevskiy2021@gmail.com>
2021-12-11 20:55:50 +0300

    Added project template

---
+++ CryptoSwiftUI/CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Valery Kavaleuski on 11.12.21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
