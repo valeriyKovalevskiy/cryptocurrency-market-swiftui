12e63a814aac777b786472d45919c7909b6bf7a4
Valery Kavaleuski <kovalevskiy2021@gmail.com>
2021-12-11 21:26:09 +0300

    Created home view navigation header

--- CryptoSwiftUI/CryptoSwiftUI/CryptoSwiftUIApp.swift
+++ CryptoSwiftUI/CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CryptoSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
