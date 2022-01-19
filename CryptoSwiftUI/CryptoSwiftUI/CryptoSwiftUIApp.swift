8f4ffecbf5ce37846ae31913ba7a5dfaad004e7d
Valery Kavaleuski <kovalevskiy2021@gmail.com>
2022-01-19 14:36:53 +0300

    designed launch screen

--- CryptoSwiftUI/CryptoSwiftUI/CryptoSwiftUIApp.swift
+++ CryptoSwiftUI/CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -9,21 +9,31 @@ import SwiftUI
 
 @main
 struct CryptoSwiftUIApp: App {
-    
-    @StateObject private var viewModel = HomeViewModel()
-    
-    init() {
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
-    }
-    
-    var body: some Scene {
-        WindowGroup {
-            NavigationView {
-                HomeView()
-                    .navigationBarHidden(true)
-            }
-            .environmentObject(viewModel)
+  
+  @StateObject private var viewModel = HomeViewModel()
+  @State private var showLaunchView: Bool = true
+  init() {
+    UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
+    UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      ZStack {
+        NavigationView {
+          HomeView()
+            .navigationBarHidden(true)
         }
+        .environmentObject(viewModel)
+        
+        ZStack {
+          if showLaunchView {
+            LaunchView(showLaunchView: $showLaunchView)
+              .transition(.move(edge: .leading))
+          }
+        }
+        .zIndex(2.0)
+      }
     }
+  }
 }
